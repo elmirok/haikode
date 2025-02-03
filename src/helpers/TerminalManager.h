d0763b7c94132da146144aa918059a27186b506c
Freaxed <andrea.anzani@gmail.com>
2025-02-03 16:29:06 +0100

    first terminal integration in Genio!

---
+++ src/helpers/TerminalManager.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright 2025, Andrea Anzani 
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#pragma once
+
+
+#include <SupportDefs.h>
+#include <Archivable.h>
+#include <View.h>
+
+class TerminalManager {
+public:
+	static BView*	CreateNewTerminal(BRect frame);
+private:
+				TerminalManager();
+		bool	IsValid() { return fId > -1; }
+	image_id	fId;
+};
+
+
