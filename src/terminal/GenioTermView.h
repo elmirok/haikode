d910cafbf5956ce840b918a71c0a3a2213b95730
Freaxed <andrea.anzani@gmail.com>
2025-02-03 12:54:45 +0100

    imported genio terminal code

---
+++ src/terminal/GenioTermView.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2025, Andrea Anzani 
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#pragma once
+
+
+#include <SupportDefs.h>
+#include "TermScrollView.h"
+
+class GenioTermView : public TermScrollView {
+public:
+
+	static	BArchivable*		Instantiate(BMessage* data);
+
+private:
+
+};
+
+
