9f03da85d7bfad6294e5c6a3c3025a92f06d114d
Jackburton79 <stefano.ceccherini@gmail.com>
2026-02-21 22:02:01 +0100

    Added TODO comment. Updated copyright

--- src/lsp-client/LSPReaderThread.cpp
+++ src/lsp-client/LSPReaderThread.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023, Andrea Anzani 
+ * Copyright 2023-2026, Andrea Anzani 
  * All rights reserved. Distributed under the terms of the MIT license.
  */
 
@@ -9,16 +9,17 @@
 
 LSPReaderThread::LSPReaderThread(LSPPipeClient& looper)
 	:
-	GenericThread("LSPReaderThread"),
+	GenericThread("LSPReaderThread", B_NORMAL_PRIORITY),
 	fTransport(looper)
 {
+	 // TODO: Maybe experiment with other priorities
 }
 
 
 status_t
 LSPReaderThread::ExecuteUnit()
 {
-	if (fTransport.readStep() == false) {
+	if (!fTransport.readStep()) {
 		Quit();
 		fTransport.Close();  //TODO: this should be performed by the Client itself not from the thread!
 		return B_ERROR;
