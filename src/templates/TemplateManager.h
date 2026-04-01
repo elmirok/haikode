016d2910808afd142149a6e33867a1a2ecb8a989
Jackburton79 <stefano.ceccherini@gmail.com>
2026-04-01 14:34:35 +0200

    Fix arguments naming mismatch

--- src/templates/TemplateManager.h
+++ src/templates/TemplateManager.h
@@ -17,7 +17,7 @@ public:
 	static status_t		CopyFileTemplate(const entry_ref* source, const entry_ref* destination, entry_ref* newFileRef);
 	static status_t		CopyProjectTemplate(const entry_ref* source, const entry_ref* destination,
 												const char* name = nullptr);
-	static status_t		CreateTemplate(const entry_ref* project);
+	static status_t		CreateTemplate(const entry_ref* file);
 	static status_t		CreateNewFolder(const entry_ref* destination, entry_ref* newFolderRef);
 
 	static BString		GetDefaultTemplateDirectory();
