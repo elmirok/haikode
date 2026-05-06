a324fd9a8cb42c629c41221d5ca540592eeae422
Jackburton79 <stefano.ceccherini@gmail.com>
2026-05-06 15:21:34 +0200

    Use placeholders in all templates
    Hope I didn't forget any

--- data/templates/BApp with window, menu, filepanel, settings/App.cpp
+++ data/templates/BApp with window, menu, filepanel, settings/App.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024, My Name 
+ * Copyright ${date.year}, ${author.name}
  * All rights reserved. Distributed under the terms of the MIT license.
  */
 
@@ -13,7 +13,7 @@
 #undef B_TRANSLATION_CONTEXT
 #define B_TRANSLATION_CONTEXT "Application"
 
-const char* kApplicationSignature = "application/x-vnd.MyName-${project.name}";
+const char* kApplicationSignature = "application/x-vnd.${author.name_without_spaces}-${project.name}";
 
 
 App::App()
