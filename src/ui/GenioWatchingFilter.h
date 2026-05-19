78dc2389e162bdf83edb1bed45608175739203ec
Jackburton79 <stefano.ceccherini@gmail.com>
2026-05-19 11:36:49 +0200

    Fix style/copyright year

--- src/ui/GenioWatchingFilter.h
+++ src/ui/GenioWatchingFilter.h
@@ -1,18 +1,18 @@
 /*
- * Copyright 2023, Andrea Anzani
+ * Copyright 2023-2026, Andrea Anzani
  * All rights reserved. Distributed under the terms of the MIT license.
  */
-#ifndef GenioWatchingFilter_H
-#define GenioWatchingFilter_H
-
+#pragma once
 
 #include <Directory.h>
 #include <Path.h>
 #include <PathMonitor.h>
+
 #include <cstdio>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <sstream>
+
 #include "Log.h"
 
 // This is attached to the PathMonitor class to avoid watching too many (useless) files.
@@ -21,16 +21,17 @@
 
 class GenioWatchingFilter : public BPrivate::BPathMonitor::BWatchingInterface {
 public:
-			GenioWatchingFilter(const char* watch_nodes_filters) {
-				std::stringstream ss(watch_nodes_filters);
-				std::string item;
-				while (std::getline(ss, item, ',')) {
-					if (!item.empty()) {
-						fFilters.push_back(item);
-						LogTraceF("Using filter [%s]\n", item.c_str());
-					}
-				}
+	GenioWatchingFilter(const char* watchNodesFilters)
+	{
+		std::stringstream ss(watchNodesFilters);
+		std::string item;
+		while (std::getline(ss, item, ',')) {
+			if (!item.empty()) {
+				fFilters.push_back(item);
+				LogTraceF("Using filter [%s]\n", item.c_str());
 			}
+		}
+	}
 
 	status_t WatchNode(const node_ref* node, uint32 flags, const BHandler* handler,
 							  		const BLooper* looper = NULL)
@@ -49,11 +50,10 @@ public:
 		dir.GetEntry(&entry);
 		BPath path;
 		entry.GetPath(&path);
-
 		BString fullPath = path.Path();
 
-		//Let's filter useless directories.
-		for(auto& filter : fFilters) {
+		// Let's filter useless directories.
+		for (auto& filter : fFilters) {
 			if (fullPath.FindFirst(filter.c_str()) != B_ERROR) {
 				LogTraceF("Filtering %s", fullPath.String());
 				return B_OK;
@@ -61,9 +61,7 @@ public:
 		}
 
 		status = BPrivate::BPathMonitor::BWatchingInterface::WatchNode(node, flags, handler, looper);
-
 		if (status != B_OK && flags != B_STOP_WATCHING) {
-
 			LogErrorF("Can't watch_node for directory [%s](%d) (%s) handler (%p) looper (%p) %d",
 						fullPath.String(),
 						node->node,
@@ -81,9 +79,6 @@ public:
 	uint32	WatchedNodesCount() const { return fWatchedNodes; }
 
 private:
-
 		uint32	fWatchedNodes = 0L;
 		std::vector<std::string>	fFilters;
 };
-
-#endif // GenioWatchingFilter_H
