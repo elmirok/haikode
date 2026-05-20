0f2ff677db5708e3d07019771e30213dc0a13a7b
Jackburton79 <stefano.ceccherini@gmail.com>
2026-05-20 14:37:23 +0200

    GitRepository: add explicit conversion from int to float

--- src/git/GitRepository.cpp
+++ src/git/GitRepository.cpp
@@ -66,7 +66,7 @@ namespace Genio::Git {
 
 		BMessage msg(progressData->what);
 		msg.AddString("progress_text", progressString);
-		msg.AddFloat("progress_value", currentProgress);
+		msg.AddFloat("progress_value", float(currentProgress));
 		progressData->messenger.SendMessage(&msg);
 		return 0;
 	}
